30f4d4d6d807f792d13da85d91ab371fd02a1f71
Jady Tippetts <jady11@gmail.com>
2021-02-16 11:37:27 -0700

    Styled home page with image

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,12 +10,12 @@ export default function NavBar() {
                     <NavLink to="/" exact 
                     activeClassName="text-white"
                     className="inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest">
-                        Jady
+                        Jady Tippetts
                     </NavLink>
                     <NavLink to="/post" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
                     activeClassName="text-red-100 bg-red-700"
                     >
-                        Blog Posts
+                        About Me
                     </NavLink>
                     <NavLink to="/project" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
                     activeClassName="text-red-100 bg-red-700"
@@ -25,12 +25,13 @@ export default function NavBar() {
                     <NavLink to="/about" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
                     activeClassName="text-red-100 bg-red-700"
                     >
-                        About Me!
+                        Blog Posts
                     </NavLink>
                 </nav>
                 <div className="inline-flex py-3 px-3 my-6">
                     <SocialIcon url="https://www.linkedin.com/in/jady-tippetts-b7a5b1106/" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35 }} />
                     <SocialIcon url="https://github.com/Jady11" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35 }} />
+                    <SocialIcon url="https://www.facebook.com/jady.tippetts/" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35 }} />
                     <SocialIcon url="https://www.instagram.com/jady11/" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35 }} />
                     
                 </div>
