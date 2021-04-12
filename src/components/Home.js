011c05be884e1c583e66af61d40202244d0785ae
Jady Tippetts <jady11@gmail.com>
2021-04-12 17:05:13 -0600

    changes to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ export default function Home() {
         <main>
             <img src={image} alt="Laos" className="absolute object-cover w-full h-full"/>
             <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-                <h1 className="text-6xl text-red-600 font-bold cursive leading-none lg:leading-snug home-name">Jady Tippetts</h1>
+                <h1 className="text-6xl text-black-600 font-bold cursive leading-none lg:leading-snug home-name">Jady Tippetts</h1>
             </section>
         </main>
     )
