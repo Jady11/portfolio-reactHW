ad73c07bbadff4d147e00720b7d195b2f3762d5e
Jady Tippetts <jady11@gmail.com>
2021-05-23 12:29:32 -0600

    rebuilt sanity

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import sanityClient from '../client';
-import wat from '../wat.jpeg';
+// import wat from '../wat.jpeg';
 import imageUrlBuilder from '@sanity/image-url';
 import BlockContent from '@sanity/block-content-to-react';
 import {SocialIcon} from 'react-social-icons';
@@ -28,9 +28,9 @@ export default function About() {
 
     return (
         <main className="relative">
-            <img src={wat}
+            {/* <img src={wat}
                 alt="Wat"
-                className="absolute w-full"/>
+                className="absolute w-full"/> */}
             <div className="p-10 lg:pt-48 container mx-auto relative">
                 <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
                     <img src={
