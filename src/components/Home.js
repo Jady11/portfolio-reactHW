15c7eee52cfd8dacb660761dde569ce0b8a8961a
Jady Tippetts <jady11@gmail.com>
2021-05-23 13:19:16 -0600

    changed test

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ export default function Home() {
         <main>
             <img src={image} alt="Laos" className="test absolute object-cover w-full h-full"/>
             <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-                <h1 className="text-6xl text-black-600 font-bold cursive leading-none lg:leading-snug home-name">Jady -test- Tippetts</h1>
+                <h1 className="text-6xl text-black-600 font-bold cursive leading-none lg:leading-snug home-name">Jady Tippetts</h1>
             </section>
         </main>
     )
